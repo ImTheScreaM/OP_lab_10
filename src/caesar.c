17943c3a2dbdff8e3f0baeabe6029b195f858d67
ImTheScreaM <gambolanddarvin@mail.ru>
2026-05-27 19:02:40 +0300

    create caesar decryption func

--- src/caesar.c
+++ src/caesar.c
@@ -5,10 +5,12 @@
 #include "../include/shift.h"
 #include "../include/structure.h"
 
-encrypt_status caesar_encryption(const char *input,
+encrypt_status caesar_encryption(
+	                    const char *input,
                       char *output,
                       size_t output_size,
-                      int shift) {
+                      int shift)
+{
 	uint8_t ret_res = ENCRYPT_OK;
 
 	ret_res = !input ?
@@ -34,6 +36,47 @@ encrypt_status caesar_encryption(const char *input,
 		case ERR_ENCRYPT_NULL_STR:
 			printf("Строка для сдвига должна быть вписана\n");
 		 	break;
+		case ERR_ENCRYPT_INVALID_ARG:
+			printf("Сдвиг должен быть указан \n");
+			break;
+		default:
+			printf("Неизвестная ошибка \n");
+	}
+
+	return ret_res;
+}
+
+encrypt_status caesar_decryption(
+                    const char *input,
+										char *output,
+										size_t output_size,
+										int shift)
+{
+	uint8_t ret_res = ENCRYPT_OK;
+
+	ret_res = !input ?
+						ERR_ENCRYPT_NULL_STR
+						: ENCRYPT_OK;
+
+	ret_res = !shift ?
+						ERR_ENCRYPT_INVALID_ARG
+						: ENCRYPT_OK;
+
+
+	switch (ret_res) {
+		case ENCRYPT_OK:
+			size_t ind = 0;
+
+			for (ind;
+					input[ind] != '\0';
+					ind++) {
+				output[ind] = (char)shift_decrypt_char(input[ind],shift);
+					}
+			output[ind] = '\0';
+			break;
+		case ERR_ENCRYPT_NULL_STR:
+			printf("Строка для сдвига должна быть вписана\n");
+			break;
 		case ERR_ENCRYPT_INVALID_ARG:
 			printf("Сдвиг должен быть указан \n");
 			break;
