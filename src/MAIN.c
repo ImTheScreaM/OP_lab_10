2b647661785326b43fb0b6f1beff7b8463c52e03
ImTheScreaM <gambolanddarvin@mail.ru>
2026-05-27 21:00:03 +0300

    update MAIN func

--- src/MAIN.c
+++ src/MAIN.c
@@ -25,30 +25,82 @@
 |    ./app                                                                                |
 |-----------------------------------------------------------------------------------------*/
 
+#include "../include/structure.h"
+
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+
 
 #define OK 0
 #define MAX_STR_SIZE 1024
+#define MAX_KEY_SIZE 26
 
 int main() {
+	uint8_t ret_res = ENCRYPT_OK;
+
 	char entered_str[MAX_STR_SIZE];
+	char caesar_encrypt[MAX_STR_SIZE];
+	char caesar_decrypt[MAX_STR_SIZE];
+	char vigener_encrypt[MAX_STR_SIZE];
+	char vigener_decrypt[MAX_STR_SIZE];
 
-	char caesar_encryption[MAX_STR_SIZE];
-	char caesar_decryption[MAX_STR_SIZE];
-	char vigener_encryption[MAX_STR_SIZE];
-	char vigener_decryption[MAX_STR_SIZE];
-
-
-	uint8_t ret_res = OK;
-	printf("ВВедите строку \n");
+	int shift;
+	char key[MAX_KEY_SIZE];
 
 
+	printf("Введите строку \n");
+	fgets(entered_str,sizeof(entered_str),stdin);
+	entered_str[strcspn(entered_str, "\n")] = '\0';
+
+	printf("Введите сдвиг \n");
+	scanf("%d",&shift);
+	getchar();
+
+	printf("Введите ключ \n");
+	fgets(key,sizeof(key),stdin);
+	key[strcspn(key, "\n")] = '\0';
+
+	printf("Исходня строка: %s\n",entered_str);
 
 
-	fgets(str,sizeof(str),stdin);
 
-	printf("%s ваша строка",str);
+	printf("----- Шифр Цезаря -----\n");
+	ret_res = caesar_encryption(entered_str, caesar_encrypt, MAX_STR_SIZE, shift);
+	if (ret_res == ENCRYPT_OK) {
+		printf("Зашифрованная строка с помощью шифра Цезаря: %s\n",caesar_encrypt);
+
+		ret_res = caesar_decryption(caesar_encrypt, caesar_decrypt, MAX_STR_SIZE, shift);
+
+		if (ret_res == ENCRYPT_OK) {
+			printf("Расшифровано: %s\n",caesar_decrypt);
+
+			if (strcmp(entered_str,caesar_decrypt) == 0) {
+				printf("Текст успешно восстановлен\n");
+			} else {
+				printf("Текст не был восстановлен");
+			}
+
+		}
+	}
+
+	printf("----- Шифр Винжера -----\n");
+	ret_res = vigener_encryption(entered_str, vigener_encrypt, MAX_STR_SIZE, key);
+	if (ret_res == ENCRYPT_OK) {
+		printf("Зашифрованная строка с помощью шифра Вижера: %s\n",vigener_encrypt);
+
+		ret_res = vigener_decryption(vigener_encrypt, vigener_decrypt, MAX_STR_SIZE, key);
+
+		if (ret_res == ENCRYPT_OK) {
+			printf("Расшифровано: %s\n",vigener_decrypt);
+
+			if (strcmp(entered_str,vigener_decrypt) == 0) {
+				printf("Текст успешно восстановлен\n");
+			} else {
+				printf("Текст не был восстановлен\n");
+			}
+		}
+	}
 
 
 	return OK;
