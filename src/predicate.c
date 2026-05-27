0ffc00abe457ecb2ce683946a27a4a9567298822
ImTheScreaM <gambolanddarvin@mail.ru>
2026-05-27 20:48:54 +0300

    create is_valid_key func

--- src/predicate.c
+++ src/predicate.c
@@ -1,3 +1,26 @@
-//
-// Created by miracle on 5/27/26.
-//
+
+#include <stdint.h>
+#include <string.h>
+
+#include "../include/predicate.h"
+
+#include <ctype.h>
+
+bool is_valid_key(const char *key) {
+	uint8_t ret_res = 1;
+	size_t ind = 0;
+
+	if (!key || strlen(key) == 0) {
+		ret_res = 0;
+	}
+
+	for (ind;
+		  key[ind] != '\0';
+		  ind++) {
+		if (!isalpha(key[ind])) {
+			ret_res = 0;
+		}
+	}
+
+	return ret_res;
+}
